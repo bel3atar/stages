6e53e5242bc349e51bd963efe2c47461a1628932
BELATAR Mohammed Ousama <bel3atar@aol.com>
2013-05-01 21:49:04 +0100

    Do not load unneeded javascript

--- views/_footer.php
+++ views/_footer.php
@@ -1,7 +1,9 @@
 				</div>
 			</div>
 		</div>
-		<script src="<?= URL ?>assets/javascripts/jquery.js"></script>
-		<script src="<?= URL ?>assets/bootstrap/js/bootstrap.min.js"></script>
+		<? if (Session::get('logged')): ?>
+			<script src="<?= URL ?>assets/javascripts/jquery.js"></script>
+			<script src="<?= URL ?>assets/bootstrap/js/bootstrap.min.js"></script>
+		<? endif ?>
 	</body>
 </html>
